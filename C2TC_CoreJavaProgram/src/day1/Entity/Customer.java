ca39c45a5bfd315bc8752cde9c09b71d275852b8
HP <HP@Lekha>
2025-07-21 16:38:00 +0530

    this is my program

---
+++ C2TC_CoreJavaProgram/src/day1/Entity/Customer.java
@@ -0,0 +1,31 @@
+package day1.Entity;
+
+public class Customer {
+	
+	int cid;
+	String name;
+	String city;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+@Override
+public String toString() {
+	return "Customer [Customer id=" + cid + ", Customer name=" + name + ", Customer city=" + city + "]";
+}
+}
--- C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
@@ -1,6 +1,6 @@
 package day1.basics;
 
-public class SampleDemo {
+public class SampleDemo{
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java class..");
