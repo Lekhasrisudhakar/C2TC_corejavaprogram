471b4ffd7267bf4d99dbbd021da02b439c3b5565
HP <HP@Lekha>
2025-07-21 19:34:26 +0530

    this is my sample program

---
+++ C2TC_CoreJavaProgram/src/day1/basics/SampleProg.java
@@ -0,0 +1,25 @@
+	package day1.basics;
+
+public class SampleProg {
+
+    public static void main() {
+		System.out.println("Welcome form main 1");
+		}
+	public static void main(char c[])
+	{
+		System.out.println("Welcome form main 2");
+	}
+	public static void main(String ar)
+	{
+		System.out.println("Welcome from main 3");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to JFS");
+		System.out.println("nextLine");
+	}
+}
+
+	
+
+
