5bcba1b9087d7096df98de5b1769eed0a7bd22ae
HP <HP@Lekha>
2025-07-24 14:41:46 +0530

    this is my day four program

--- C2TC_CoreJavaProgram/src/dayfour/firstpackage/PersonDemo.java
+++ C2TC_CoreJavaProgram/src/dayfour/firstpackage/PersonDemo.java
@@ -1,5 +1,7 @@
 package dayfour.firstpackage;
-import java.util.*;
+
+import java.util.Scanner;
+
 public class PersonDemo {
 	public static void main(String args[]) {
 		Person p1 = new Person(); // default constructor invoked
@@ -12,7 +14,7 @@ public class PersonDemo {
 		age = sc.nextInt();
 		sc.nextLine();
 		city = sc.nextLine();
-	
+		// p1.personName=name private member can't accessible
 		p1.setPersonName(name);
 		p1.setPersonAge(age);
 		p1.setPersonCity(city);
@@ -24,11 +26,10 @@ public class PersonDemo {
 		age = sc.nextInt();
 		sc.nextLine();
 		city = sc.nextLine();
-		p1 = new Person(name, age, city); 
+		p1 = new Person(name, age, city); // parameterized constructor invoked);
 		System.out.println("Person Details Name : " + p1.getPersonName() + "\tAge : " + p1.getPersonAge() + "\tCity : "
 				+ p1.getPersonCity());
 		sc.close();
 
 	}
 }
-
