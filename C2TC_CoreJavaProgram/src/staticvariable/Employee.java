3e74a1fbbe05d9a7a57e40f0a48d8496881733b1
HP <HP@Lekha.mshome.net>
2025-07-25 15:41:12 +0530

    this is my employee program

---
+++ C2TC_CoreJavaProgram/src/staticvariable/Employee.java
@@ -0,0 +1,13 @@
+package staticvariable;
+
+public class Employee {
+	private String name;
+	private int id;	
+	static String companyName = "TNS";
+
+	Employee(String name, int id) {
+		this.name = name;
+		this.id = id;
+	}
+
+}
